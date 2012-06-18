6d1ff2f2e3acaf2e8afc91d596563cc71397f9a2
Sebastian Wiesner <lunaryorn@googlemail.com>
2012-06-18 18:06:37 +0200

    Re-phrase sphinxcontrib docstring
    The extension is not detached from the sphinxcontrib distribution, hence this
    package should not refer to sphinxcontrib anymore

--- sphinxcontrib/__init__.py
+++ sphinxcontrib/__init__.py
@@ -3,11 +3,7 @@
     sphinxcontrib
     ~~~~~~~~~~~~~
 
-    This package is a namespace package that contains all extensions
-    distributed in the ``sphinx-contrib`` distribution.
-
-    :copyright: Copyright 2007-2009 by the Sphinx team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
+    Contains 3rd party Sphinx extensions.
 """
 
 __import__('pkg_resources').declare_namespace(__name__)
