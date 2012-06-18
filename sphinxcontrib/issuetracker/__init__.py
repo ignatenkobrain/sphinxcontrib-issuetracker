7634dcf52e925e5ad08c07ca98bbbf2cc4cc71e3
Sebastian Wiesner <lunaryorn@googlemail.com>
2012-06-18 20:21:32 +0200

    Bump to 0.10.1

--- sphinxcontrib/issuetracker/__init__.py
+++ sphinxcontrib/issuetracker/__init__.py
@@ -40,7 +40,7 @@ from __future__ import (print_function, division, unicode_literals,
                         absolute_import)
 
 
-__version__ = '0.10'
+__version__ = '0.10.1'
 
 import sys
 import re
