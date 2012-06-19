15444be7115e797ce00a23743d07a1825dce31b3
Sebastian Wiesner <lunaryorn@googlemail.com>
2012-06-19 21:53:30 +0200

    Update copyright year

--- sphinxcontrib/issuetracker/resolvers.py
+++ sphinxcontrib/issuetracker/resolvers.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2010, 2011, Sebastian Wiesner <lunaryorn@googlemail.com>
+# Copyright (c) 2010, 2011, 2012 Sebastian Wiesner <lunaryorn@googlemail.com>
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
