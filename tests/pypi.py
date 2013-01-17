5f6dcd8a26feb551facc7c52271b33bdaf81db97
Sebastian Wiesner <lunaryorn@gmail.com>
2013-01-17 11:53:56 +0100

    Fix encoding cookie

--- tests/pypi.py
+++ tests/pypi.py
@@ -1,6 +1,5 @@
-# -*- coding: utf8 -*-
-#
-# Copyright (c) 2009 Python Software Foundation
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009, 2013 Python Software Foundation
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
